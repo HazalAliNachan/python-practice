edbfe85c418f5cde5bc5a6932a22f5856a914809
HazalAliNachan <hazalalinachan@gmail.com>
2026-08-23 16:28:36 +0000

    Added Operators & Math practice questions

---
+++ Operators & Math/p26.py
@@ -0,0 +1,6 @@
+base = int(input("Enter the base: "))
+exp = int(input("Enter the exp: "))
+result =1
+for i in range(exp):
+    result = result*base
+print(result)    
---
+++ Operators & Math/p27.py
@@ -0,0 +1,6 @@
+num = input("Enter the number: ")
+121
+if num==num[::-1]:
+    print("Number is Palindrome")
+else:
+    print("Number is Not Palindrome")    
